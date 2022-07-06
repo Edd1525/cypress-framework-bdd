f27cb5a69c4b843e66789504593fa80a2d75a189
EdwardBustamante <edward.bustamante@fineos.com>
2022-07-06 17:13:45 -0500

    enable video

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,17 +1,33 @@
 const report = require("multiple-cucumber-html-reporter");
+const os = require("os");
+const dayjs = require("dayjs");
 
 report.generate({
   jsonDir: "cypress/cucumber-json", // ** Path of .json file **//
   reportPath: "./reports/cucumber-multi-report",
+
   metadata: {
-    browser: {
-      name: "chrome",
-      version: "81",
-    },
-    device: "Local test machine",
+    device: os.hostname(),
     platform: {
-      name: "Linux",
-      version: "1.0",
+      name: os.platform(),
+      version: os.version,
     },
   },
+
+  customData: {
+    title: "Cypress Cucumber Html Report",
+    data: [
+      {
+        label: "Execution Start Time",
+        value: dayjs().format("YYYY-MM-DD HH:mm:ss.SSS"),
+      },
+      {
+        label: "Execution End Time",
+        value: dayjs().format("YYYY-MM-DD HH:mm:ss.SSS"),
+      },
+    ],
+  },
+  pageTitle: "Cypress Cucumber Html Report",
+  openReportInBrowser: true,
+  displayDuration: true,
 });
